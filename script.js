1d7defcf2f04dcb9f74c59c493ff1ee882740d1b
Claire <berkagmpp@gmail.com>
2022-01-21 15:52:38 +1300

    ass 2 end

---
+++ script.js
@@ -0,0 +1,51 @@
+// sticky nav manu with the hamburger icon
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+    navbar.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+}
+cancelBtn.onclick = () => {
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+}
+window.onscroll = () => {
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+// footer: copyright auto generation
+let copyYear = "2021";
+let now = new Date;
+let curYear = now.getFullYear();
+let credit = document.getElementById("text-muted");
+if (copyYear == curYear) credit.innerHTML = copyYear + ", The HAUKAI";
+else credit.innerHTML = copyYear + "-" + curYear + ", The HAUKAI";
+
+// contact page: submit button click
+window.onload = function () {  // event run after entire page loading, including its content
+    document.getElementById("submit").addEventListener("click", function(event) {
+        event.preventDefault(); // prevent opening the URL
+        let name = document.getElementById("cus_name");
+        let email = document.getElementById("cus_email");
+        let content = document.getElementById("cus_content");
+        if (name.value == "" || email.value == "" || content.value == "") {
+            alert("Please enter all boxes.");
+            return false;
+        } else {
+            if (confirm ("Are you sure to submit?\n\n Name: " + name.value
+                + "\n Email: " + email.value + "\n Content: \n" + content.value) == true) {
+                    // below would be change for the backend integration
+                    name.value = "";
+                    email.value = "";
+                    content.value = "";
+                    alert("complete.");
+            } else {
+                return false;
+            }
+        }
+    })
+}
